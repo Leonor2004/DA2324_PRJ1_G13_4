d753fbdef68efe72cb4a44e99cbafd01e26d114b
Leonor2004 <up202205796@up.pt>
2024-02-28 09:46:28 +0000

    Doxyfile for documentation correction & main.cpp added

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by leono on 28/02/2024.
+//
+/**
+ * @file main.cpp
+ *
+ * @mainpage DA project1
+ * @section intro_sec Introduction
+ * This project was made in the context of the Algorithm Design class.
+ *
+ * DESCRIÇÃO DO PROJETOOOOOOOOO
+ *
+ * This project was made by: Filipa Fidalgo (up202208039) and Leonor Couto (up202205796).
+ */
+
+/**
+ * @brief Main function to initialize data and run the program.
+ *
+ * Complexity: O(1)
+ *
+ * @return Program exit status.
+ */
+int main(){
+    return 0;
+}
